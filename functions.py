8a4e6f6f8040b0e760cd1edbc345f55519af79cf
n0t10n <0ts.notion@gmail.com>
2022-02-19 14:05:39 +0100

    Adding functions

--- functions.py
+++ functions.py
@@ -1,5 +1,4 @@
 import numpy as np
-from pandas import array
 
 class BattleShip:
 
@@ -27,13 +26,13 @@ class BattleShip:
                 point = tuple(np.random.randint((board.shape[1]+1, board.shape[2]+1)))
             
             return (int(matrix),) + point
+
         except UnboundLocalError as uble:
-            print(f"""
-{uble}
-Possible errors:
-    -Matrix value out of bounds
-    -Board type not array
-    -Board dimensions not equal to two
+            print(f"""{uble}
+                Possible errors:
+                    -Matrix value out of bounds
+                    -Board type not array
+                    -Board dimensions not equal to two
             """)
     
     def neighbor(board, position=tuple):
@@ -45,6 +44,7 @@ Possible errors:
             -position: type tuple. The position where the function will work on.
         
         Errors:
+            -IndexError: if the value position is out of board shape.
 
         Return:
             A dictionary with the position argument as key,
@@ -54,19 +54,49 @@ Possible errors:
         pos_list = []
         pos = {}
 
-        # Above
-        if board[p[0], p[1]-1, p[2]] == " ":
-            pos_list.append(tuple((p[0],) + (p[1]-1,) + (p[2],)))
-        # Below
-        if board[p[0], p[1]+1, p[2]] == " ":
-            pos_list.append(tuple((p[0],) + (p[1]+1,) + (p[2],)))
-        # Left
-        if board[p[0], p[1], p[2]-1] == " ":
-            pos_list.append(tuple((p[0],) + (p[1],) + (p[2]-1,)))
-        # Right
-        if board[p[0], p[1], p[2]+1] == " ":
-            pos_list.append(tuple((p[0],) + (p[1],) + (p[2]+1,)))
+        try:    
+            # Above
+            if p[1] > 0:
+                if board[p[0], p[1]-1, p[2]] == " ":
+                    pos_list.append(tuple((p[0],) + (p[1]-1,) + (p[2],)))
+            # Below
+            if p[1] < 9:
+                if board[p[0], p[1]+1, p[2]] == " ":
+                    pos_list.append(tuple((p[0],) + (p[1]+1,) + (p[2],)))
+            # Left
+            if p[2] > 0:
+                if board[p[0], p[1], p[2]-1] == " ":
+                    pos_list.append(tuple((p[0],) + (p[1],) + (p[2]-1,)))
+            # Right
+            if p[2] < 9:
+                if board[p[0], p[1], p[2]+1] == " ":
+                    pos_list.append(tuple((p[0],) + (p[1],) + (p[2]+1,)))
+            # Above-right
+            if p[1] > 0 and p[2] < 9:
+                if board[p[0], p[1]-1, p[2]+1] == " ":
+                    pos_list.append(tuple((p[0],) + (p[1]-1,) + (p[2]+1,)))
+            # Above-left
+            if p[1] > 0 and p[2] > 0:
+                if board[p[0], p[1]-1, p[2]-1] == " ":
+                    pos_list.append(tuple((p[0],) + (p[1]-1,) + (p[2]-1,)))
+            # Below-right
+            if p[1] < 9 and p[2] < 9:
+                if board[p[0], p[1]+1, p[2]+1] == " ":
+                    pos_list.append(tuple((p[0],) + (p[1]+1,) + (p[2]+1,)))
+            # Below-left
+            if p[1] < 9 and p[2] > 0:
+                if board[p[0], p[1]+1, p[2]-1] == " ":
+                    pos_list.append(tuple((p[0],) + (p[1]+1,) + (p[2]-1,)))
 
-        pos[position] = pos_list
+            pos[position] = pos_list
 
-        return pos
+            return pos
+
+        except IndexError as ie:
+            print(f"""{ie}
+                REMEMBER: your board is shape {board.shape}.
+                    -Your board must have a shape of (2, 10, 10)
+                    -The position must belong within the board bonds
+            """)
+
+        
