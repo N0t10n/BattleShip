bd680326ea5c63997cc5be4918e9536de742420a
n0t10n <0ts.notion@gmail.com>
2022-02-22 01:34:10 +0100

    fixing

--- functions.py
+++ functions.py
@@ -23,40 +23,36 @@ class BattleShip:
         self.board = full((2, 10, 10), " ")
         self.boats = {1:4, 2:3, 3:2, 4:1}
     
-    def rand_point(board, matrix=int):
+    def rand_point(self, matrix=int):
         """
         Calculates a random point in the array.
 
         Parameters:
-            -board: type array. A 3 dimensional matrix.
             -matrix: type int. The matrix where the point is generated.
         
         Errors:
             -matrix value out of bounds
-            -board type not array
-            -board dimensions not equal to two
 
         Return:
             Returns a random point in the given matrix of type tuple.
         """
         try:
-            if int(matrix) < board.shape[0]:
-                point = (int(matrix),) + tuple(random.randint((board.shape[1]+1, board.shape[2]+1)))
+            if int(matrix) < self.board.shape[0]:
+                point = (int(matrix),) + tuple(random.randint((self.board.shape[1], self.board.shape[2])))
                 
-                if board[point] == " ":
+                if self.board[point] == " ":
                     return point
 
                 else:
-                    BattleShip.rand_point(board, matrix)
+                    BattleShip.rand_point(self, matrix)
             
             else:
                 raise IndexError
 
         except IndexError:
             print("""Possible errors:
-    -Matrix value out of bounds
-    -Board type not array
-    -Board dimensions not equal to two""")
+    -Matrix value out of bounds""")
+            BattleShip.rand_point(self, matrix)
             
     
     def neighbor(board, position=tuple):
@@ -78,43 +74,47 @@ class BattleShip:
         pos_list = []
         pos = {}
 
-        try:    
-            # Above
-            if p[1] > 0:
-                if board[p[0], p[1]-1, p[2]] == " ":
-                    pos_list.append(tuple((p[0],) + (p[1]-1,) + (p[2],)))
-            # Below
-            if p[1] < 9:
-                if board[p[0], p[1]+1, p[2]] == " ":
-                    pos_list.append(tuple((p[0],) + (p[1]+1,) + (p[2],)))
-            # Left
-            if p[2] > 0:
-                if board[p[0], p[1], p[2]-1] == " ":
-                    pos_list.append(tuple((p[0],) + (p[1],) + (p[2]-1,)))
-            # Right
-            if p[2] < 9:
-                if board[p[0], p[1], p[2]+1] == " ":
-                    pos_list.append(tuple((p[0],) + (p[1],) + (p[2]+1,)))
-            # Above-right
-            if p[1] > 0 and p[2] < 9:
-                if board[p[0], p[1]-1, p[2]+1] == " ":
-                    pos_list.append(tuple((p[0],) + (p[1]-1,) + (p[2]+1,)))
-            # Above-left
-            if p[1] > 0 and p[2] > 0:
-                if board[p[0], p[1]-1, p[2]-1] == " ":
-                    pos_list.append(tuple((p[0],) + (p[1]-1,) + (p[2]-1,)))
-            # Below-right
-            if p[1] < 9 and p[2] < 9:
-                if board[p[0], p[1]+1, p[2]+1] == " ":
-                    pos_list.append(tuple((p[0],) + (p[1]+1,) + (p[2]+1,)))
-            # Below-left
-            if p[1] < 9 and p[2] > 0:
-                if board[p[0], p[1]+1, p[2]-1] == " ":
-                    pos_list.append(tuple((p[0],) + (p[1]+1,) + (p[2]-1,)))
+        try:
+            if board[p] == "O":
+                # Above
+                if p[1] > 0:
+                    if board[p[0], p[1]-1, p[2]] == " ":
+                        pos_list.append(tuple((p[0],) + (p[1]-1,) + (p[2],)))
+                # Below
+                if p[1] < 9:
+                    if board[p[0], p[1]+1, p[2]] == " ":
+                        pos_list.append(tuple((p[0],) + (p[1]+1,) + (p[2],)))
+                # Left
+                if p[2] > 0:
+                    if board[p[0], p[1], p[2]-1] == " ":
+                        pos_list.append(tuple((p[0],) + (p[1],) + (p[2]-1,)))
+                # Right
+                if p[2] < 9:
+                    if board[p[0], p[1], p[2]+1] == " ":
+                        pos_list.append(tuple((p[0],) + (p[1],) + (p[2]+1,)))
+                # Above-right
+                if p[1] > 0 and p[2] < 9:
+                    if board[p[0], p[1]-1, p[2]+1] == " ":
+                        pos_list.append(tuple((p[0],) + (p[1]-1,) + (p[2]+1,)))
+                # Above-left
+                if p[1] > 0 and p[2] > 0:
+                    if board[p[0], p[1]-1, p[2]-1] == " ":
+                        pos_list.append(tuple((p[0],) + (p[1]-1,) + (p[2]-1,)))
+                # Below-right
+                if p[1] < 9 and p[2] < 9:
+                    if board[p[0], p[1]+1, p[2]+1] == " ":
+                        pos_list.append(tuple((p[0],) + (p[1]+1,) + (p[2]+1,)))
+                # Below-left
+                if p[1] < 9 and p[2] > 0:
+                    if board[p[0], p[1]+1, p[2]-1] == " ":
+                        pos_list.append(tuple((p[0],) + (p[1]+1,) + (p[2]-1,)))
 
-            pos[position] = pos_list
+                pos[position] = pos_list
 
-            return pos
+                return pos
+
+            else:
+                BattleShip.neighbor(board, position)
 
         except IndexError as ie:
             print(f"""{ie}
@@ -123,90 +123,64 @@ class BattleShip:
                 -The position must belong within the board bonds
             """)
 
-    def direction(board, position=tuple, length=int):
+    def direction(self, position=tuple, length=int):
         """
         Calculates within the board bondaries, all possible directions
-        to locate a boat with length 'length'.
+        to locate a boat with length 'length' and gives a single direction as output.
 
         Parameters:
-            -board: type array. A 3 dimensional matrix
             -position: type tuple. The start boat coordinates.
             -length: boat length as integer.
 
         Return:
-            A list of all possible directions mentioned:
+            Randomly a possible directions mentioned as string:
             North, South, West, East
         """
         p = position
         l = int(length - 1)
         c = {"N":0, "S":0, "E":0, "W":0}
 
-        try:
-            if l > 0:
-                if board[p] != "O":
-                    # Vertical axis
-                    # Down
-                    if p[1]+l < 10:
-                        x = board[p[0], p[1]:p[1]+l, p[2]] == " "
-                        for i in x:
-                            if i == True:
-                                c['S'] += 1
-                    # Up
-                    if p[1]-l >= 0:
-                        x = board[p[0], p[1]:p[1]-l:-1, p[2]] == " "
-                        for i in x:
-                            if i == True:
-                                c['N'] += 1
+        if self.board[p] != "O":
+            # Vertical axis
+            # Down
+            if p[1]+l < 10:
+                x = self.board[p[0], p[1]:p[1]+l, p[2]] == " "
+                for i in x:
+                    if i == True:
+                        c['S'] += 1
+            # Up
+            if p[1]-l >= 0:
+                x = self.board[p[0], p[1]:p[1]-l:-1, p[2]] == " "
+                for i in x:
+                    if i == True:
+                        c['N'] += 1
 
-                    # Horizontal axis
-                    # Right
-                    if p[2]+l < 10:
-                        x = board[p[0], p[1], p[2]:p[2]+l] == " "
-                        for i in x:
-                            if i == True:
-                                c['E'] += 1
-                    # Left
-                    if p[2]-l >= 0:
-                        x = board[p[0], p[1], p[2]:p[2]-l:-1] == " "
-                        for i in x:
-                            if i == True:
-                                c['W'] += 1
-                    
-                    # Here compares which direction is equal to the length to be listed in "d"
-                    d = [k for k, v in c.items() if v == l]
+            # Horizontal axis
+            # Right
+            if p[2]+l < 10:
+                x = self.board[p[0], p[1], p[2]:p[2]+l] == " "
+                for i in x:
+                    if i == True:
+                        c['E'] += 1
+            # Left
+            if p[2]-l >= 0:
+                x = self.board[p[0], p[1], p[2]:p[2]-l:-1] == " "
+                for i in x:
+                    if i == True:
+                        c['W'] += 1
+            
+            # Here compares which direction is equal to the length to be listed in "d"
+            d = [k for k, v in c.items() if v == l]
 
-                    return d
-            else:
-                raise ValueError
+            # With the all possible directions now randomly stores one of them
+            rand_d = random.choice(d)
 
-        except IndexError as ie:
-            print(f"""{ie}
-            REMEMBER: your board is shape {board.shape}.
-                -The board indexing stars by 0, so at the ninth index is a total of 10 indexes.
-                -Your board must have a shape of (2, 10, 10).
-                -The position must belong within the board bonds.
-            """)
-        except ValueError:
-            print("""The length must be an int number equal or higher than 0 and smaller than 9.""")
+            return rand_d
 
-    def rand_direction(board, position=tuple, length=int):
-        """
-        With the possible directions extracted from the function 'direction',
-        this function chooses a random direction from the output (list).
+        else:
+            print("There's no directions availabe")
 
-        Parameters:
-            -board: type array. A 3 dimensional matrix
-            -position: type tuple. The start boat coordinates.
-            -length: boat length as integer.
-        
-        Return:
-            A string.
-        """
-        directions = BattleShip.direction(board, position, length)
-        d = random.choice(directions)
-        return d
-
-    def boat_placer(board, position=tuple, length=int, direction=str):
+    def boat_placer(self, position=tuple, length=int, direction=str):
         """
         Rewrites the array board with the placed boat.
 
@@ -228,14 +202,14 @@ class BattleShip:
         # So for 'N' and 'W' directions we must slice rather in positive
         if d == "N":
             f_p = (p[0], p[1]+1-l, p[2])
-            board[p[0], p[1]:p[1]-l:-1, p[2]] = "O"
+            self.board[p[0], p[1]:p[1]-l:-1, p[2]] = "O"
         elif d == "S":
-            board[p[0], p[1]:p[1]+l, p[2]] = "O"
+            self.board[p[0], p[1]:p[1]+l, p[2]] = "O"
         elif d == "W":
             f_p = (p[0], p[1], p[2]+1-l)
-            board[p[0], p[1], f_p[2]:p[2]+1] = "O"
+            self.board[p[0], p[1], f_p[2]:p[2]+1] = "O"
         else: # d == "E":
-            board[p[0], p[1], p[2]:p[2]+l] = "O"
+            self.board[p[0], p[1], p[2]:p[2]+l] = "O"
 
     def shot(self, enemy, difficulty='easy'):
         """
@@ -287,12 +261,16 @@ class BattleShip:
                 print("Coordinates already chosen before, try again.")
                 BattleShip.shot(self, enemy)
     
-    def boat_placer_loop(board, point, boats):
-        if boats[4]
-            for k, v in boats.items():
+    def boat_placer_loop(self):
+        
+        for k, v in self.boats.items():
+            for i in range(v):
                 # Random points
-                u_p = BattleShip.rand_point(board, 0)
+                u_p = BattleShip.rand_point(self, 0)
                 # Random possible directions
-                u_d = BattleShip.direction(board, u_p, v)
+                u_d = BattleShip.direction(self, u_p, k)
                 # Placing it
-                BattleShip.boat_placer(board, u_p, v, u_d)
+                BattleShip.boat_placer(self, u_p, k, u_d)
+            
+
+        
--- main.py
+++ main.py
@@ -10,33 +10,5 @@ print("Generating boards...")
 sleep(1)
 
 # Boat length 4
-# Random points
-u_p = bs.rand_point(user.board, 0)
-m_p = bs.rand_point(machine.board, 0)
-# Random possible directions
-u_d = bs.direction(user.board, u_p, user.boats[4])
-m_d = bs.direction(machine.board, m_p, machine.boats[4])
-# Placing it
-bs.boat_placer(user.board, u_p, user.boats[4], u_d)
-bs.boat_placer(machine.board, m_p, machine.boats[4], m_d)
-
-# 2 boats length 3
-u_p_3 = bs.rand_point(user.board, 0)
-m_p_3 = bs.rand_point(machine.board, 0)
-# Random possible directions
-u_d_3 = bs.direction(user.board, u_p_3, user.boats[4])
-m_d_3 = bs.direction(machine.board, m_p_3, machine.boats[4])
-# Placing it
-bs.boat_placer(user.board, u_p_3, user.boats[4], u_d)
-bs.boat_placer(machine.board, m_p_3, machine.boats[4], m_d)
-
-for k, v in user.boats.items():
-    # Random points
-    u_p = bs.rand_point(user.board, 0)
-    m_p = bs.rand_point(machine.board, 0)
-    # Random possible directions
-    u_d = bs.direction(user.board, u_p, v)
-    m_d = bs.direction(machine.board, m_p, v)
-    # Placing it
-    bs.boat_placer(user.board, u_p, v, u_d)
-    bs.boat_placer(machine.board, m_p, machine.boats[4], m_d)
+user.boat_placer_loop()
+print(user.board)
